4dc0979221257ec01358420d407679f9b26bd346
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:07 +0200

    Complete Car Journey Planner practice program

---
+++ CSharp/1.Basics/2.Variables/Car Journey Planner/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Distance (km): ");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Average speed (km/h): ");
+double speed = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Fuel efficiency (km/L): ");
+double efficiency = Convert.ToDouble(Console.ReadLine());
+
+double hours = distance / speed;
+double litres = distance / efficiency;
+
+Console.WriteLine();
+Console.WriteLine($"Travel Time : {hours:F2} hours");
+Console.WriteLine($"Fuel Needed : {litres:F2} litres");
