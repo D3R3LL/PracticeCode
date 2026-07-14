4bf35f1ae68ecc372fa8e6e60c0bf76554f108c5
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:53:01 +0200

    Age Check practice program

---
+++ CSharp/1.Basics/3.Control Flow/If Statements/Age Check/Program.cs
@@ -0,0 +1,10 @@
+﻿// Ask the user for their age
+Console.Write("Enter your age: ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+// Only print this message if the condition is true
+if (age >= 18)
+{
+    Console.WriteLine("You are an adult.");
+}
