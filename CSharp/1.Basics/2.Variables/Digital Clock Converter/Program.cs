4f93ae3e027289c387916e431868ad6290d7c749
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:08 +0200

    Complete Digital Clock Converter practice program

---
+++ CSharp/1.Basics/2.Variables/Digital Clock Converter/Program.cs
@@ -0,0 +1,11 @@
+﻿// Digital Clock Converter
+
+Console.Write("Enter total seconds: ");
+int totalSeconds = Convert.ToInt32(Console.ReadLine());
+
+int hours = totalSeconds / 3600;
+int minutes = (totalSeconds % 3600) / 60;
+int seconds = totalSeconds % 60;
+
+Console.WriteLine();
+Console.WriteLine($"{hours:D2}:{minutes:D2}:{seconds:D2}");
