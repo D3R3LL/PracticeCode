77f2b339a0e72cedac644b8e5e62bc95b1028a1b
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:08 +0200

    Complete Distance Between Two Points practice program

---
+++ CSharp/1.Basics/2.Variables/Distance Between Two Points/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter x1: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter y1: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter x2: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter y2: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+// Distance formula
+double distance = Math.Sqrt(
+    Math.Pow(x2 - x1, 2) +
+    Math.Pow(y2 - y1, 2));
+
+Console.WriteLine();
+Console.WriteLine($"Distance = {distance:F2}");
