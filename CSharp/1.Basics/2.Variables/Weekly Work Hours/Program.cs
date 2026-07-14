d6e54f3aeff465856e6b4f23d7093df208e6fff8
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:16 +0200

    Complete Weekly Work Hours practice program

---
+++ CSharp/1.Basics/2.Variables/Weekly Work Hours/Program.cs
@@ -0,0 +1,23 @@
+﻿// Weekly Work Hours
+
+Console.Write("Hours worked Monday: ");
+double monday = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Hours worked Tuesday: ");
+double tuesday = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Hours worked Wednesday: ");
+double wednesday = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Hours worked Thursday: ");
+double thursday = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Hours worked Friday: ");
+double friday = Convert.ToDouble(Console.ReadLine());
+
+double totalHours = monday + tuesday + wednesday + thursday + friday;
+double average = totalHours / 5;
+
+Console.WriteLine();
+Console.WriteLine($"Total Hours: {totalHours:F1}");
+Console.WriteLine($"Average Per Day: {average:F1}");
