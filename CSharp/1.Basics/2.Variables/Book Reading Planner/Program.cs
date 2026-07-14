3d9d966c2395a433734716cc8c6402e98adb14b7
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:07 +0200

    Complete Book Reading Planner practice program

---
+++ CSharp/1.Basics/2.Variables/Book Reading Planner/Program.cs
@@ -0,0 +1,12 @@
+﻿// Book Reading Planner
+
+Console.Write("Number of pages: ");
+int pages = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Pages read each day: ");
+int pagesPerDay = Convert.ToInt32(Console.ReadLine());
+
+double days = (double)pages / pagesPerDay;
+
+Console.WriteLine();
+Console.WriteLine($"It will take approximately {days:F1} days to finish the book.");
