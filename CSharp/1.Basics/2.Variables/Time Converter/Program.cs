3daf0d680ea6a761090761f03777979803bf2267
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:15 +0200

    Complete Time Converter practice program

---
+++ CSharp/1.Basics/2.Variables/Time Converter/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter minutes: ");
+int minutes = Convert.ToInt32(Console.ReadLine());
+
+int hours = minutes / 60;
+int remainingMinutes = minutes % 60;
+
+Console.WriteLine();
+Console.WriteLine($"{minutes} minutes = :");
+Console.WriteLine($"{hours} hour(s)");
+Console.WriteLine($"{remainingMinutes} minute(s)");
