ab46e74b40d96118adffddd881c8691177d9a39b
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:08 +0200

    Complete Currency Converter practice program

---
+++ CSharp/1.Basics/2.Variables/Currency Converter/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Amount in USD: ");
+double usd = Convert.ToDouble(Console.ReadLine());
+
+double exchangeRate = 27.50;
+
+double zigEquivalent = usd * exchangeRate;
+
+Console.WriteLine($"Converted amount = {zigEquivalent:F2}zig");
