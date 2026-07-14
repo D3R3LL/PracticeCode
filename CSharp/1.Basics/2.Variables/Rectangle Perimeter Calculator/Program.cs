ab404d614b5059feeca953504ffa39d9d13c7505
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:13 +0200

    Complete Rectangle Perimeter Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Rectangle Perimeter Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Length: ");
+double length = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Width: ");
+double width = Convert.ToDouble(Console.ReadLine());
+
+// formula for perimeter
+
+double perimeter = 2 * (length + width);
+
+Console.WriteLine($"Perimeter = {perimeter}");
+
