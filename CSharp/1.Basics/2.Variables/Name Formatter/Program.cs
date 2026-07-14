3d2e8bb0d13c18494c8936c8678f7650a8f04555
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:12 +0200

    Complete Name Formatter practice program

---
+++ CSharp/1.Basics/2.Variables/Name Formatter/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("First name: ");
+string? firstName = Console.ReadLine();
+
+Console.Write("Last name: ");
+string? lastName = Console.ReadLine();
+
+string fullName = $"{firstName} {lastName}";
+
+Console.WriteLine($"\nFull name: {fullName}");
+Console.WriteLine($"\nWelcome, {fullName}!");
