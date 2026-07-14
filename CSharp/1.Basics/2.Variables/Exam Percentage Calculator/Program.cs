03f50727ded3c39553c12d9eb3876ae6b65641aa
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:09 +0200

    Complete Exam Percentage Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Exam Percentage Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿// Exam Percentage Calculator
+
+Console.Write("Marks obtained: ");
+double marks = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Total marks: ");
+double totalMarks = Convert.ToDouble(Console.ReadLine());
+
+// Calculate percentage
+double percentage = (marks / totalMarks) * 100;
+
+Console.WriteLine();
+Console.WriteLine("===== Exam Result =====");
+Console.WriteLine($"Marks      : {marks}");
+Console.WriteLine($"Total Marks: {totalMarks}");
+Console.WriteLine($"Percentage : {percentage:F2}%");
