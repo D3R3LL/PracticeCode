23558ad0176aadfd6d489cdf322223ea4abfd677
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:15 +0200

    Complete Swap Two Numbers practice program

---
+++ CSharp/1.Basics/2.Variables/Swap Two Numbers/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("First number: ");
+int first = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Second number: ");
+int second = Convert.ToInt32(Console.ReadLine());
+
+// Temporary variable used during the swap
+int temp = first;
+
+first = second;
+second = temp;
+
+Console.WriteLine();
+
+Console.WriteLine($"First = {first}");
+Console.WriteLine($"Second = {second}");
