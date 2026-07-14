7c185a39003be70fc6f5260703fadd1b8d13ce85
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:05 +0200

    Complete Area of a circle practice program

---
+++ CSharp/1.Basics/2.Variables/Area of a circle/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the radius: ");
+double radius = Convert.ToDouble(Console.ReadLine());
+
+//Area = π × r²
+
+double area = Math.PI * radius * radius;
+
+Console.WriteLine($"Area of a circle = {area:F2}");
