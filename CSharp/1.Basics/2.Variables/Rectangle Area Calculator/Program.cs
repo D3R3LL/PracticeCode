6368912f48aa36cf82394f1c267e0c649613ffa5
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:13 +0200

    Complete Rectangle Area Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Rectangle Area Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿// Rectangle Area Calculator
+
+Console.Write("Enter length: ");
+double length = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Enter width: ");
+double width = Convert.ToDouble(Console.ReadLine());
+
+// Area = length × width
+double area = length * width;
+
+Console.WriteLine($"The area of the rectangle is {area} square units.");
+
