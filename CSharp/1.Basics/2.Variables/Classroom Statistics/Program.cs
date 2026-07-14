1bcdb9b1e8dcaf9bcf2a50ec07da6ad09f2247da
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:07 +0200

    Complete Classroom Statistics practice program

---
+++ CSharp/1.Basics/2.Variables/Classroom Statistics/Program.cs
@@ -0,0 +1,17 @@
+﻿// Classroom Statistics
+
+Console.Write("Number of boys: ");
+int boys = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Number of girls: ");
+int girls = Convert.ToInt32(Console.ReadLine());
+
+int totalStudents = boys + girls;
+
+double boyPercentage = (double)boys / totalStudents * 100;
+double girlPercentage = (double)girls / totalStudents * 100;
+
+Console.WriteLine();
+Console.WriteLine($"Total Students : {totalStudents}");
+Console.WriteLine($"Boys : {boyPercentage:F1}%");
+Console.WriteLine($"Girls: {girlPercentage:F1}%");
