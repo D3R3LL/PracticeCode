2ca87c510478ef776b1bb82a00462488e54a871f
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:15 +0200

    Complete Temperature Report practice program

---
+++ CSharp/1.Basics/2.Variables/Temperature Report/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Temperature in Celsius: ");
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+// Convert temperatures
+double fahrenheit = (celsius * 9 / 5) + 32;
+double kelvin = celsius + 273.15;
+
+Console.WriteLine();
+Console.WriteLine($"Celsius    : {celsius:F2} °C");
+Console.WriteLine($"Fahrenheit : {fahrenheit:F2} °F");
+Console.WriteLine($"Kelvin     : {kelvin:F2} K");
