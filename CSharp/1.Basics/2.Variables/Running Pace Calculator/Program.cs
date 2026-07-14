ed16cc7fe32998ae6be98fe92ff4b2a1a3674fde
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:14 +0200

    Complete Running Pace Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Running Pace Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// Running Pace Calculator
+
+Console.Write("Distance (km): ");
+double distance = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Time (minutes): ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+double pace = time / distance;
+
+Console.WriteLine();
+Console.WriteLine($"Average pace: {pace:F2} minutes per kilometre");
