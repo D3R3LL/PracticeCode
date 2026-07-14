b87e3258ae0386ffd2ed8d783e8d19648573721a
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:14 +0200

    Complete Seconds to Hours, Minutes and Seconds practice program

---
+++ CSharp/1.Basics/2.Variables/Seconds to Hours, Minutes and Seconds/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter the number of seconds: ");
+int totalSeconds = Convert.ToInt32(Console.ReadLine());
+
+int hours = totalSeconds / 3600;
+int minutes = (totalSeconds % 3600) / 60;
+int seconds = totalSeconds % 60;
+
+Console.WriteLine($"{hours} hour(s), {minutes} minute(s), {seconds} second(s)");
