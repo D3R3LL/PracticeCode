a2d11e3b755f0d1b458103a3a7b8f657d22f2dcc
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:06 +0200

    Complete Basic User input Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Basic User input Calculator/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int Addition = num1 + num2;
+int Subtraction = num1 - num2;
+int Multiplication = num1 * num2;
+int Division = num1 / num2;
+
+Console.WriteLine();
+
+Console.WriteLine($"Addition Answer: {Addition}");
+Console.WriteLine($"Subtraction Answer: {Subtraction}");
+Console.WriteLine($"Multiplication Answer: {Multiplication}");
+Console.WriteLine($"Division Answer: {Division}");
