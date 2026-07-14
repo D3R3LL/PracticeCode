df2789f68db5eb82a7c9aa0984c21987ce3cc115
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:08 +0200

    Complete Computer Storage Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Computer Storage Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿// Computer Storage Calculator
+
+Console.Write("Storage size (GB): ");
+double gigabytes = Convert.ToDouble(Console.ReadLine());
+
+double megabytes = gigabytes * 1024;
+double kilobytes = megabytes * 1024;
+
+Console.WriteLine();
+Console.WriteLine($"Gigabytes : {gigabytes}");
+Console.WriteLine($"Megabytes : {megabytes:F0}");
+Console.WriteLine($"Kilobytes : {kilobytes:F0}");
