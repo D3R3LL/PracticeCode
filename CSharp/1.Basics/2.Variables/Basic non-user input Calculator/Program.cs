beb56c79f6b5f4ca9f87cd847f615dcdfe2ffaf4
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:06 +0200

    Complete Basic non-user input Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Basic non-user input Calculator/Program.cs
@@ -0,0 +1,13 @@
+﻿int num1 = 10;
+int num2 = 5;
+
+int addition = num1 + num2;
+int subtraction = num1 - num2;
+int multiplication = num1 * num2;
+int division = num1 / num2;
+
+Console.WriteLine($"Addition: {addition}");
+Console.WriteLine($"Subtraction: {subtraction}");
+Console.WriteLine($"Multiplication: {multiplication}");
+Console.WriteLine($"Division: {division}");
+
