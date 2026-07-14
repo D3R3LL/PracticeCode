23d5a0e56216e2de632668104a957b062a5df8b4
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:06 +0200

    Complete Body Mass Index (BMI) Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Body Mass Index (BMI) Calculator/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Weight (kg): ");
+double weight = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Height (m): ");
+double height = Convert.ToDouble(Console.ReadLine());
+
+//BMI = Weight ÷ (Height × Height)
+
+double bmi = weight / (height * height);
+
+Console.WriteLine($"BMI = {bmi:F2}");
