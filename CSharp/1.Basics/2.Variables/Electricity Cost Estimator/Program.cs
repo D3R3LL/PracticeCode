54514a40fda719f059ff570305fc7a761cccc9d3
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:09 +0200

    Complete Electricity Cost Estimator practice program

---
+++ CSharp/1.Basics/2.Variables/Electricity Cost Estimator/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Appliance power (Watts): ");
+double watts = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Hours used per day: ");
+double hours = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Electricity price per kWh: ");
+double price = Convert.ToDouble(Console.ReadLine());
+
+double kilowatts = watts / 1000;
+double dailyCost = kilowatts * hours * price;
+double monthlyCost = dailyCost * 30;
+
+Console.WriteLine();
+Console.WriteLine($"Daily Cost: ${dailyCost:F2}");
+Console.WriteLine($"Monthly Cost: ${monthlyCost:F2}");
