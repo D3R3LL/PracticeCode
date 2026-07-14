2f7dc6e810abfff90da893c9102b0aee29d14a3c
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:10 +0200

    Complete File Download Time Estimator practice program

---
+++ CSharp/1.Basics/2.Variables/File Download Time Estimator/Program.cs
@@ -0,0 +1,12 @@
+﻿// File Download Time Estimator
+
+Console.Write("File size (MB): ");
+double fileSize = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Download speed (MB/s): ");
+double speed = Convert.ToDouble(Console.ReadLine());
+
+double seconds = fileSize / speed;
+
+Console.WriteLine();
+Console.WriteLine($"Estimated download time: {seconds:F2} seconds");
