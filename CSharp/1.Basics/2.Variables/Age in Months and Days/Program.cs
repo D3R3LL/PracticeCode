057ff60128224303e13d918da2c0b010b21c5416
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:05 +0200

    Complete Age in Months and Days practice program

---
+++ CSharp/1.Basics/2.Variables/Age in Months and Days/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Enter your age in years: ");
+int age = Convert.ToInt32(Console.ReadLine());
+
+int months = age * 12;
+int days = age * 365;
+
+Console.WriteLine($"Months: {months}");
+Console.WriteLine($"Approximate days: {days}");
