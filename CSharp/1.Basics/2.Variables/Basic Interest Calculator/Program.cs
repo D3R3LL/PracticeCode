472718c3fb1bc99c48c24c2f6d1d7d74b94962ad
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:05 +0200

    Complete Basic Interest Calculator practice program

---
+++ CSharp/1.Basics/2.Variables/Basic Interest Calculator/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Principal: ");
+double principal = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Rate (%): ");
+double rate = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Time (years): ");
+double time = Convert.ToDouble(Console.ReadLine());
+
+//Interest = Principal × Rate × Time / 100
+
+double interest = principal * rate * time / 100;
+
+Console.WriteLine($"Simple Interest = ${interest:F2}");
