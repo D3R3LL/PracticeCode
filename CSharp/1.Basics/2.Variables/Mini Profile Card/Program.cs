35192ed351802122383f501c8031307e4596bef8
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:11 +0200

    Complete Mini Profile Card practice program

---
+++ CSharp/1.Basics/2.Variables/Mini Profile Card/Program.cs
@@ -0,0 +1,21 @@
+﻿// Mini Profile Card
+
+Console.Write("Full Name: ");
+string? name = Console.ReadLine();
+
+Console.Write("Occupation: ");
+string? occupation = Console.ReadLine();
+
+Console.Write("Country: ");
+string? country = Console.ReadLine();
+
+Console.Write("Favourite Programming Language: ");
+string? language = Console.ReadLine();
+
+Console.WriteLine();
+Console.WriteLine("=====Profile Card=============");
+Console.WriteLine($"Name      : {name}");
+Console.WriteLine($"Occupation: {occupation}");
+Console.WriteLine($"Country   : {country}");
+Console.WriteLine($"Language  : {language}");
+Console.WriteLine("==============================");
