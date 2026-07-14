bf392e65a5044ffb8c98676b0adc9e1dd3e591f8
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:05 +0200

    Complete Average of Five Numbers practice program

---
+++ CSharp/1.Basics/2.Variables/Average of Five Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Number 1: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 2: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 3: ");
+double n3 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 4: ");
+double n4 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Number 5: ");
+double n5 = Convert.ToDouble(Console.ReadLine());
+
+double average = (n1 + n2 + n3 + n4 + n5) / 5;
+
+Console.WriteLine($"\nAverage = {average:F2}");
