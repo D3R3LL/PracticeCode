447bc2210b0053dea4f3cd7c2d5cbee53395e5b0
Darrel <darreltadiwalugube@gmail.com>
2026-07-14 20:42:16 +0200

    Complete Water Intake Tracker practice program

---
+++ CSharp/1.Basics/2.Variables/Water Intake Tracker/Program.cs
@@ -0,0 +1,15 @@
+﻿// Water Intake Tracker
+
+Console.Write("Glasses of water per day: ");
+int glasses = Convert.ToInt32(Console.ReadLine());
+
+int weekly = glasses * 7;
+int monthly = glasses * 30;
+int yearly = glasses * 365;
+
+Console.WriteLine();
+Console.WriteLine("===== Water Intake =====");
+Console.WriteLine($"Daily   : {glasses} glasses");
+Console.WriteLine($"Weekly  : {weekly} glasses");
+Console.WriteLine($"Monthly : {monthly} glasses");
+Console.WriteLine($"Yearly  : {yearly} glasses");
